50a30f1c200b3083a0bdd88610db5d343fc51d8b
tuygun <tahauygun@gmail.com>
2018-03-09 03:07:38 -0600

    graphql initiated

---
+++ schema.js
@@ -0,0 +1,12 @@
+const {
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLInt,
+    GraphQLSchema,
+    GraphQLList,
+    GraphQLNonNUll
+} = require('graphql');
+
+module..exports = new GraphQLSchema({
+
+});
